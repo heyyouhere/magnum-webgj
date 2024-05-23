f5298e9725e56f78974bb1caf88e1f201a9ae3e6
Obuchash <obuchashki@gmail.com>
2024-05-23 15:15:05 +0300

    upd3

--- src/blocks/ThreeBackground/Scene.jsx
+++ src/blocks/ThreeBackground/Scene.jsx
@@ -4,7 +4,7 @@ import Spline from '@splinetool/react-spline';
 export default function Scene() {
   return (
     <div id="three_canvas">
-        <Spline scene="/backgrpund.splinecode" />
+        <Spline scene="/background.splinecode"/>
     </div>
 
   );
